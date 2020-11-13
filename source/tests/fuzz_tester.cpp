a35aaafc5c5046facd7d0a6d9e8dba92b755ff3d
ndoxx <bg.physique@gmail.com>
2020-11-13 10:24:44 +0100

    Updated fuzz tester, updated install rules

--- source/tests/fuzz_tester.cpp
+++ source/tests/fuzz_tester.cpp
@@ -13,20 +13,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     ap::ArgParse parser("nuclear", "0.1");
     parser.set_exit_on_special_command(false); // Prevent exiting on --help and --version
-
-    parser.add_flag('A', "param_A", "The parameter A");
-    parser.add_flag('B', "param_B", "The parameter B");
-    parser.add_flag('C', "param_C", "The parameter C");
     parser.add_flag('x', "param_x", "The parameter x");
     parser.add_flag('y', "param_y", "The parameter y");
     parser.add_flag('z', "param_z", "The parameter z");
+    parser.add_list<int>('l', "list_l", "A list of values");
     parser.add_variable<int>('m', "var_m", "The variable m", 10);
-    parser.add_variable<int>('n', "var_n", "The variable n", 10);
-    parser.add_variable<float>('o', "var_o", "The variable o", 10);
     parser.add_positional<int>("MAGIC", "The magic number");
-    parser.set_flags_exclusive({'x', 'y'});
-    parser.set_flags_exclusive({'y', 'z'});
-    parser.set_variables_exclusive({'m', 'o'});
+    parser.set_dependency('y', 'x');
 
     auto arguments = tc::tokenize(reinterpret_cast<const char*>(data), size);
     auto argv = tc::make_argv(arguments);
