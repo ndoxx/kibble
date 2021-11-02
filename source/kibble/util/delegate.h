7b8dafed8e212754d32c5379310c61d2bea367a1
ndoxx <bg.physique@gmail.com>
2021-11-02 07:51:11 +0100

    Working on better delegates

---
+++ source/kibble/util/delegate.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <exception>
+#include <type_traits>
+
+namespace kb
+{
+struct BadDelegateCallException : public std::exception
+{
+    const char *what() const throw()
+    {
+        return "Cannot invoke a member function without a class instance";
+    }
+};
+
+template <typename Signature> class Delegate;
+
+template <typename R, typename... Args> class Delegate<R(Args...)>
+{
+public:
+    Delegate() = default;
+
+    Delegate(const Delegate &other) = default;
+    auto operator=(const Delegate &other) -> Delegate & = default;
+
+    template <typename... UArgs, typename = std::enable_if_t<std::is_invocable_v<R(Args...), UArgs...>>>
+    auto operator()(UArgs &&...args) -> R
+    {
+        return (*stub_)(instance_, std::forward<UArgs>(args)...);
+    }
+
+    template <auto Function, typename = std::enable_if_t<std::is_invocable_r_v<R, decltype(Function), Args...>>>
+    auto bind() -> void
+    {
+        instance_ = nullptr;
+        stub_ = static_cast<stub_function>(
+            [](const void *, Args... args) -> R { return std::invoke(Function, std::forward<Args>(args)...); });
+    }
+
+    template <auto MemberFunction, typename Class,
+              typename = std::enable_if_t<std::is_invocable_r_v<R, decltype(MemberFunction), const Class *, Args...>>>
+    auto bind(const Class *cls) -> void
+    {
+        instance_ = cls;
+        stub_ = static_cast<stub_function>([](const void *p, Args... args) -> R {
+            const auto *c = static_cast<const Class *>(p);
+            return std::invoke(MemberFunction, c, std::forward<Args>(args)...);
+        });
+    }
+
+    template <auto MemberFunction, typename Class,
+              typename = std::enable_if_t<std::is_invocable_r_v<R, decltype(MemberFunction), Class *, Args...>>>
+    auto bind(Class *cls) -> void
+    {
+        instance_ = cls;
+        stub_ = static_cast<stub_function>([](const void *p, Args... args) -> R {
+            auto *c = const_cast<Class *>(static_cast<const Class *>(p));
+            return std::invoke(MemberFunction, c, std::forward<Args>(args)...);
+        });
+    }
+
+private:
+    using stub_function = R (*)(const void *, Args...);
+
+    [[noreturn]] static auto stub_null(const void *, Args...) -> R
+    {
+        throw BadDelegateCallException{};
+    }
+
+    const void *instance_ = nullptr;
+    stub_function stub_ = &stub_null;
+};
+} // namespace kb
