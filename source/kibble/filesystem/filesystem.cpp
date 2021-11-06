384fa5d718126da8745d1256b8b4452af6df1c16
ndoxx <bg.physique@gmail.com>
2021-11-06 01:14:34 +0100

    Filesystem documentation

--- source/kibble/filesystem/filesystem.cpp
+++ source/kibble/filesystem/filesystem.cpp
@@ -1,5 +1,5 @@
-#include "filesystem/filesystem.h"
 #include "assert/assert.h"
+#include "filesystem/filesystem.h"
 #include "filesystem/resource_pack.h"
 #include "logger/logger.h"
 #include "string/string.h"
@@ -23,7 +23,8 @@ namespace kfs
 
 static const std::regex r_alias("^(.+?)://(.+)");
 
-template <typename TP> std::time_t to_time_t(TP tp)
+template <typename TP>
+std::time_t to_time_t(TP tp)
 {
     using namespace std::chrono;
     auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now() + system_clock::now());
@@ -32,7 +33,7 @@ template <typename TP> std::time_t to_time_t(TP tp)
 
 struct UpathParsingResult
 {
-    const DirectoryAlias* alias_entry = nullptr;
+    const DirectoryAlias *alias_entry = nullptr;
     std::string path_component;
 };
 
@@ -44,8 +45,8 @@ FileSystem::FileSystem()
 
 FileSystem::~FileSystem()
 {
-    for(auto&& [key, entry] : aliases_)
-        for(auto* packfile : entry.packfiles)
+    for (auto &&[key, entry] : aliases_)
+        for (auto *packfile : entry.packfiles)
             delete packfile;
 }
 
@@ -58,8 +59,8 @@ bool FileSystem::setup_settings_directory(std::string vendor, std::string appnam
 #ifdef __linux__
     // * Locate home directory
     // First, check the HOME environment variable, and if not set, fallback to getpwuid()
-    const char* homebuf;
-    if((homebuf = getenv("HOME")) == NULL)
+    const char *homebuf;
+    if ((homebuf = getenv("HOME")) == NULL)
         homebuf = getpwuid(getuid())->pw_dir;
 
     fs::path home_directory = fs::canonical(homebuf);
@@ -67,7 +68,7 @@ bool FileSystem::setup_settings_directory(std::string vendor, std::string appnam
 
     // Check if ~/.config/<vendor>/<appname> is applicable, if not, fallback to
     // ~/.<vendor>/<appname>/config
-    if(fs::exists(home_directory / ".config"))
+    if (fs::exists(home_directory / ".config"))
         app_settings_directory_ = home_directory / ".config" / vendor / appname;
     else
         app_settings_directory_ = home_directory / su::concat('.', vendor) / appname / "config";
@@ -76,9 +77,9 @@ bool FileSystem::setup_settings_directory(std::string vendor, std::string appnam
 #endif
 
     // If directories do not exist, create them
-    if(!fs::exists(app_settings_directory_))
+    if (!fs::exists(app_settings_directory_))
     {
-        if(!fs::create_directories(app_settings_directory_))
+        if (!fs::create_directories(app_settings_directory_))
         {
             KLOGE("ios") << "Failed to create config directory at:" << std::endl;
             KLOGI << KS_PATH_ << app_settings_directory_ << std::endl;
@@ -93,16 +94,16 @@ bool FileSystem::setup_settings_directory(std::string vendor, std::string appnam
     KLOGI << KS_PATH_ << app_settings_directory_ << std::endl;
 
     // Alias the config directory
-    if(alias.empty())
+    if (alias.empty())
         alias = "config";
     alias_directory(app_settings_directory_, alias);
 
     return true;
 }
 
-const fs::path& FileSystem::get_settings_directory()
+const fs::path &FileSystem::get_settings_directory()
 {
-    if(app_settings_directory_.empty())
+    if (app_settings_directory_.empty())
     {
         KLOGW("ios") << "Application config directory has not been setup." << std::endl;
         KLOGI << "Call setup_config_directory() after FileSystem construction." << std::endl;
@@ -111,7 +112,7 @@ const fs::path& FileSystem::get_settings_directory()
     return app_settings_directory_;
 }
 
-bool FileSystem::is_older(const std::string& unipath_1, const std::string& unipath_2)
+bool FileSystem::is_older(const std::string &unipath_1, const std::string &unipath_2)
 {
     auto path_1 = regular_path(unipath_1);
     auto path_2 = regular_path(unipath_2);
@@ -125,12 +126,12 @@ bool FileSystem::is_older(const std::string& unipath_1, const std::string& unipa
     return (cftime_2 > cftime_1);
 }
 
-bool FileSystem::alias_directory(const fs::path& _dir_path, const std::string& alias)
+bool FileSystem::alias_directory(const fs::path &_dir_path, const std::string &alias)
 {
     // Maybe the path was specified with proximate syntax (../) -> make it lexically normal and absolute
     fs::path dir_path(fs::canonical(_dir_path));
 
-    if(!fs::exists(dir_path))
+    if (!fs::exists(dir_path))
     {
         KLOGE("ios") << "Cannot add directory alias. Directory does not exist:" << std::endl;
         KLOGI << KS_PATH_ << dir_path << std::endl;
@@ -140,9 +141,9 @@ bool FileSystem::alias_directory(const fs::path& _dir_path, const std::string& a
 
     hash_t alias_hash = H_(alias);
     auto findit = aliases_.find(alias_hash);
-    if(findit != aliases_.end())
+    if (findit != aliases_.end())
     {
-        auto& entry = findit->second;
+        auto &entry = findit->second;
         entry.alias = alias;
         entry.base = dir_path;
         KLOG("ios", 0) << "Overloaded directory alias:" << std::endl;
@@ -157,9 +158,9 @@ bool FileSystem::alias_directory(const fs::path& _dir_path, const std::string& a
     return true;
 }
 
-bool FileSystem::alias_packfile(const fs::path& pack_path, const std::string& alias)
+bool FileSystem::alias_packfile(const fs::path &pack_path, const std::string &alias)
 {
-    if(!fs::exists(pack_path))
+    if (!fs::exists(pack_path))
     {
         KLOGE("ios") << "Cannot add pack alias. File does not exist:" << std::endl;
         KLOGI << KS_PATH_ << pack_path << std::endl;
@@ -169,9 +170,9 @@ bool FileSystem::alias_packfile(const fs::path& pack_path, const std::string& al
 
     hash_t alias_hash = H_(alias);
     auto findit = aliases_.find(alias_hash);
-    if(findit != aliases_.end())
+    if (findit != aliases_.end())
     {
-        auto& entry = findit->second;
+        auto &entry = findit->second;
         entry.packfiles.push_back(new PackFile(pack_path));
     }
     else
@@ -187,31 +188,31 @@ bool FileSystem::alias_packfile(const fs::path& pack_path, const std::string& al
     return true;
 }
 
-fs::path FileSystem::regular_path(const std::string& unipath) const
+fs::path FileSystem::regular_path(const std::string &unipath) const
 {
     return to_regular_path(parse_universal_path(unipath));
 }
 
-std::string FileSystem::make_universal(const fs::path& path, hash_t base_alias_hash) const
+std::string FileSystem::make_universal(const fs::path &path, hash_t base_alias_hash) const
 {
     // Make path relative to the aliased directory
-    const auto& alias_entry = get_alias_entry(base_alias_hash);
+    const auto &alias_entry = get_alias_entry(base_alias_hash);
     auto rel_path = fs::relative(path, alias_entry.base);
     return su::concat(alias_entry.alias, "://", rel_path.string());
 }
 
-UpathParsingResult FileSystem::parse_universal_path(const std::string& unipath) const
+UpathParsingResult FileSystem::parse_universal_path(const std::string &unipath) const
 {
     UpathParsingResult result;
 
     // Try to regex match an aliasing of the form "alias://path/to/file"
     std::smatch match;
-    if(std::regex_search(unipath, match, r_alias))
+    if (std::regex_search(unipath, match, r_alias))
     {
         // Unalias
         hash_t alias_hash = H_(match[1].str());
         auto findit = aliases_.find(alias_hash);
-        if(findit != aliases_.end())
+        if (findit != aliases_.end())
         {
             result.alias_entry = &findit->second;
             result.path_component = match[2].str();
@@ -224,28 +225,28 @@ UpathParsingResult FileSystem::parse_universal_path(const std::string& unipath) 
     return result;
 }
 
-fs::path FileSystem::to_regular_path(const UpathParsingResult& result) const
+fs::path FileSystem::to_regular_path(const UpathParsingResult &result) const
 {
     // If an alias was found, path_component is relative to the aliased base directory
-    if(result.alias_entry)
+    if (result.alias_entry)
         return fs::absolute((result.alias_entry->base / result.path_component)).lexically_normal();
     // Simply return a canonical path
     return fs::absolute(result.path_component).lexically_normal();
 }
 
-IStreamPtr FileSystem::get_input_stream(const std::string& unipath, bool binary) const
+IStreamPtr FileSystem::get_input_stream(const std::string &unipath, bool binary) const
 {
     KLOG("ios", 0) << "Opening stream:" << std::endl;
     KLOGI << "universal: " << KS_PATH_ << unipath << std::endl;
 
     auto result = parse_universal_path(unipath);
     // If a pack file is aliased at this name, try to find entry in pack
-    if(result.alias_entry)
+    if (result.alias_entry)
     {
-        for(const auto* ppack : result.alias_entry->packfiles)
+        for (const auto *ppack : result.alias_entry->packfiles)
         {
             auto findit = ppack->find(result.path_component);
-            if(findit != ppack->end())
+            if (findit != ppack->end())
             {
                 KLOGI << "source: " << KS_INST_ << "pack" << std::endl;
                 return ppack->get_input_stream(findit->second);
@@ -263,7 +264,7 @@ IStreamPtr FileSystem::get_input_stream(const std::string& unipath, bool binary)
     K_ASSERT(fs::is_regular_file(filepath), "Not a file.");
 
     auto mode = std::ios::in;
-    if(binary)
+    if (binary)
         mode |= std::ios::binary;
 
     std::shared_ptr<std::ifstream> pifs(new std::ifstream(filepath, mode));
@@ -279,7 +280,7 @@ void FileSystem::init_self_path()
     ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff) - 1);
     K_ASSERT(len != -1, "Cannot read self path using readlink.");
 
-    if(len != -1)
+    if (len != -1)
         buff[len] = '\0';
     self_path = fs::path(buff);
 #else
