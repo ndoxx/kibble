6e504bb8915358df2b08f18a7575c45ad584d98a
ndoxx <bg.physique@gmail.com>
2023-03-22 22:53:03 +0100

    UID white/blacklists don't filter out severe logs anymore

--- source/kibble/logger2/policies/uid_filter.cpp
+++ source/kibble/logger2/policies/uid_filter.cpp
@@ -10,7 +10,7 @@ UIDWhitelist::UIDWhitelist(std::set<hash_t> enabled) : enabled_(enabled)
 
 bool UIDWhitelist::transform_filter(LogEntry &entry) const
 {
-    return entry.uid_text.empty() || contains(H_(entry.uid_text));
+    return entry.uid_text.empty() || int8_t(entry.severity) <= 2 || contains(H_(entry.uid_text));
 }
 
 UIDBlacklist::UIDBlacklist(std::set<hash_t> disabled) : disabled_(disabled)
@@ -19,7 +19,7 @@ UIDBlacklist::UIDBlacklist(std::set<hash_t> disabled) : disabled_(disabled)
 
 bool UIDBlacklist::transform_filter(LogEntry &entry) const
 {
-    return entry.uid_text.empty() || !contains(H_(entry.uid_text));
+    return entry.uid_text.empty() || int8_t(entry.severity) <= 2 || !contains(H_(entry.uid_text));
 }
 
 } // namespace kb::log
